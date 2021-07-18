413c95bc705a8c88c1c3d9d296cd88e3741d8169
Yehor Pliasov <fenix4088@gmail.com>
2021-07-18 18:01:59 +0300

    CSSTransition component works with module css v1

--- src/components/SimpleLoader/SimpleLoader.jsx
+++ src/components/SimpleLoader/SimpleLoader.jsx
@@ -1,22 +1,25 @@
 import React, {useState} from 'react';
-import {Transition} from 'react-transition-group';
+import {CSSTransition, Transition} from 'react-transition-group';
 
-// import s from './SimpleLoader.module.scss';
-import './simpleloader.css'
+import s from './SimpleLoader.module.scss';
+// import './simpleloader.css'
 
 export const SimpleLoader = (props) => {
 
     const [isLoaderVisible, setIsLoaderVisible] = useState(false);
 
     return (
-        <div className={'loaderWrapper'}>
+        <div className={s.loaderWrapper}>
             <button onClick={() => setIsLoaderVisible(!isLoaderVisible)}>show</button>
-            <div className={'wrap'}>
-                <Transition in={isLoaderVisible} timeout={300} mountOnEnter unmountOnExit>
-                    {state => <div className={`circle ${state}`}/>}
-                </Transition>
+            <div className={s.wrap}>
+                <CSSTransition in={isLoaderVisible} timeout={500} classNames={{
+                    enterActive: s['circle-show'],
+                    enterDone: s['circle-rotate'],
+                    exitActive: s['circle-hide'],
+                }} mountonEnter unmountOnExit>
+                    <div className={s.circle}/>
+                </CSSTransition>
             </div>
-
 
         </div>
 
